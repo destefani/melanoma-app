8f3f8dc0de0c2cc5220ef3f6d0f4642f14bbf506
ronniejuan <hofmannronnie@gmail.com>
2022-04-18 18:01:28 +0200

    model working

--- main.py
+++ main.py
@@ -8,8 +8,25 @@ import io
 import torch
 from torchvision import transforms
 from PIL import Image 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+"http://localhost:3000/"
+]
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 # Load model
 model = torch.load('models/0020.pt', map_location='cpu')
@@ -31,7 +48,7 @@ def transform_image(image_bytes):
 
 @app.get("/")
 def hello():
-    return "Hello Mono"
+    return " Mono"
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
@@ -40,7 +57,6 @@ async def predict(file: UploadFile = File(...)):
     prediction = model(input_image)
     probability = torch.sigmoid(prediction).item()
     return {f'{int(probability * 100)}%'}
-
+    
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
+   uvicorn.run(app, host="0.0.0.0", port=3000)
